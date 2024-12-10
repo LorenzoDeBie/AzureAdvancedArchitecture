2dec4bac77636105de000723c09bb263cdb770cb
Lorenzo De Bie <Lorenzo.DeBie@fednot.be>
2024-12-10 14:46:22 +0100

    module 0 - part 2

--- module-0-prerequisites-and-introduction/2.Introduction.ps1
+++ module-0-prerequisites-and-introduction/2.Introduction.ps1
@@ -7,26 +7,27 @@
 # Step 1: Create a Resource Group
 # Use the following command to create a new resource group. Replace `<email-resource-group>` with your desired name and `<resource-group-location>` with the appropriate Azure region (e.g., `westeurope`):
 
-$emailResourceGroup = 'your-resource-group-name'
+$emailResourceGroup = 'AzureLorenzo'
 
 # List all Azure locations. Select one that is close to you.
 az account list-locations -o table
 
-$location = 'your-location'
+$location = 'West Europe'
 
-az group create --name $emailResourceGroup --location $location
+# Commented Out because Resource Group already Exists
+# az group create --name $emailResourceGroup --location $location
 
 #----------------------------------------------------------------------------------------------------------------------
 # Step 2: Deploy an Azure API Management Resource (APIM)
 
 # Deploy an APIM resource within your resource group. Replace the placeholders with your desired values:
 
-$apimName = '<your-apim-name>'
+$apimName = 'Lorenzo-APIM'
 
 # This is for receiving notifications about API subscriptions.
 # It won't be used for this exercise but you need to set it to create the resource.
-$publisherName = '<your-publisher-name>'
-$publisherEmail = '<your@publisher.email>'
+$publisherName = 'Lorenzo-Publisher'
+$publisherEmail = 'lorenzo.debie@axxes.com'
 
 az apim create `
   --name $apimName `
